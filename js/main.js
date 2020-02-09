fe6cc8de1774fb01b0e0b6d6e3522e9937e1784e
frontend90 <leon44173@gmail.com>
2020-02-09 17:41:10 +0200

    верстка

--- js/main.js
+++ js/main.js
@@ -5,3 +5,54 @@ $('.header-slider').slick({
   dots: true,
   autoplay: true
 });
+
+/***************   portfolio-project     **********/
+$('[data-modal]').click(function () {
+  event.preventDefault();
+
+  let $this = $(this);
+  let modalId = $this.data('modal');
+
+  $(modalId).addClass('show');
+});
+
+$('.close').click(function () {
+  event.preventDefault();
+
+  let $this = $(this);
+  let modalParent = $this.parents('.portfolio-project');
+
+  modalParent.removeClass('show');
+});
+
+/*************** фильтр portfolio   **************/
+$(function () {
+  $('.link a').click(function (event) {
+    event.preventDefault();
+    var get_id = this.id;
+    var get_current = $('.card.' + get_id);
+    $('.card').not(get_current).hide(500);
+    get_current.show(500);
+  });
+  $('#all').click(function () {
+    $('.card').show(500);
+  });
+});
+
+/*********************  плавный скролл    *********/
+$(document).ready(function () {
+  $('a[href^="#"]').click(function () {
+    elementClick = $(this).attr("href");
+    destination = $(elementClick).offset().top;
+    if ($.browser) {
+      $('body').animate({
+        scrollTop: destination
+      }, 1000);
+    } else {
+      $('html').animate({
+        scrollTop: destination
+      }, 1000);
+    }
+    return false;
+  });
+});
